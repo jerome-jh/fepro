abad671b22f782f30793ca4b2ffe257a6c2c6e58
jerome <jerome@jerome-pc>
2018-04-08 23:12:25 +0200

    Clean CNF logic transform

--- logic.py
+++ logic.py
@@ -1,148 +1,142 @@
 from copy import *
 
+def AND(*arg):
+    assert(len(arg) >= -AND_op.arity)
+    return [AND_op, *arg]
+
+def OR(*arg):
+    assert(len(arg) >= -OR_op.arity)
+    return [OR_op, *arg]
+
+def IMP(a, b):
+    return [IMP_op, a, b]
+
+def EQ(a, b):
+    return [EQ_op, a, b]
+
+def NOT(a):
+    return [NOT_op, a]
+
+class AND_op:
+    arity = -2
+    symb = '/\\'
+    func = AND
+    def assoc(e, *arg):
+        e.extend(arg)
+
+class OR_op:
+    arity = -2
+    symb = '\\/'
+    func = OR 
+    def assoc(self, *args):
+        self.arg.extend(args)
+
+class IMP_op:
+    arity = 2
+    symb = '->'
+    func = IMP
+
+class EQ_op:
+    arity = 2
+    symb = '='
+    func = EQ
+
+class NOT_op:
+    arity = 1
+    symb = '-'
+    func = NOT
+
+def STR(exp):
+    if Logic.islist(exp):
+        op, arg = Logic.lisp(exp)
+        if op.arity == 2:
+            return '(' + STR(arg[0]) + ' ' + op.symb + ' ' + STR(arg[1]) + ')'
+        elif op.arity == 1:
+            return op.symb + STR(arg[0])
+        elif op.arity == -2:
+            s = '(' + STR(arg[0])
+            for e in arg[1:]:
+                s += ' ' + op.symb + ' ' + STR(e) 
+            s += ')'
+            return s
+        else:
+            raise Exception('Unexpected arity')
+    else:
+        return str(exp)
+
 class Logic:
-    opcode = [ 'AND', 'OR', 'IMP', 'EQ', 'NOT' ]
-    symb = [ '/\\', '\\/', '->', '=', '-' ]
-    arity = [ -2, -2, 2, 2, 1 ]
-    opdict = dict()
-    class Op:
-        pass
-    op = Op()
-    for i, o in enumerate(opcode):
-        setattr(op, o, i)
-        opdict[o] = i
-
-    def __init__(self, exp):
-        try:
-            len(exp)
-        except:
-            raise Exception('Expression must be iterable')
-        self.exp = Logic.normalize(exp)
-
-    def AND(*args):
-        return [ Logic.op.AND, *args ]
-
-    def OR(*args):
-        return [ Logic.op.OR, *args ]
-
-    def IMP(a, b):
-        return [ Logic.op.IMP, a, b ]
-
-    def EQ(a, b):
-        return [ Logic.op.EQ, a, b ]
-
-    def NOT(e):
-        return [ Logic.op.NOT, e ]
-
     def islist(exp):
-        try:
-            len(exp)
-            return True
-        except TypeError:
-            return False
+        return type(exp) != type(int())
 
     def lisp(exp):
         return exp[0], exp[1:]
 
-    def normalize(exp):
-        if not Logic.islist(exp):
-            raise Exception("Iterable type expected")
-
-        car, cdr = Logic.lisp(exp)
-        try:
-            op = Logic.opdict[car.upper()]
-        except KeyError:
-            raise Exception("Invalid opcode '%s'"%op)
-
-        a = Logic.arity[op]
-        if a > 0:
-            if len(cdr) != a:
-                raise Exception("Opcode '%s' must have %d arguments"%(car, a))
-        elif len(cdr) < -a:
-            raise Exception("Opcode '%s' must have at least %d arguments"%(car, -a))
-
-        norm = [op]
-        for e in cdr:
-            if Logic.islist(e):
-                norm.append(Logic.normalize(e))
-            else:
-                if type(e) != type(int()):
-                    raise Exception("Argument '%d' is not an integer")
-                if e == 0:
-                    raise Exception("Arguments cannot be zero")
-                norm.append(e)
-        return norm
-
     def convert_eq(exp):
-        car, cdr = Logic.lisp(exp)
-        if car == Logic.op.EQ:
-            a = cdr[0]
-            b = cdr[1]
+        """ exp is a list """
+        op, arg = Logic.lisp(exp)
+        if op.func == EQ:
+            a = arg[0]
+            b = arg[1]
             if Logic.islist(a):
                 a = Logic.convert_eq(a)
             if Logic.islist(b):
                 b = Logic.convert_eq(b)
-            return [Logic.op.AND, [Logic.op.IMP, a, b], [Logic.op.IMP, copy(b), copy(a)]]
+            return AND(IMP(a, b), IMP(copy(b), copy(a)))
         else:
-            for i, e in enumerate(cdr):
-                if Logic.islist(e):
-                    exp[i+1] = Logic.convert_eq(e)
+            for i, a in enumerate(arg):
+                if Logic.islist(a):
+                    exp[i+1] = Logic.convert_eq(a)
             return exp
 
     def convert_imp(exp):
-        car, cdr = Logic.lisp(exp)
-        if car == Logic.op.IMP:
-            a = cdr[0]
-            b = cdr[1]
+        """ exp is a list """
+        op, arg = Logic.lisp(exp)
+        if op.func == IMP:
+            a = arg[0]
+            b = arg[1]
             if Logic.islist(a):
                 a = Logic.convert_imp(a)
             if Logic.islist(b):
                 b = Logic.convert_imp(b)
-            return [Logic.op.OR, [Logic.op.NOT, a], b]
+            return OR(NOT(a), b)
         else:
-            for i, e in enumerate(cdr):
-                if Logic.islist(e):
-                    exp[i+1] = Logic.convert_imp(e)
+            for i, a in enumerate(arg):
+                if Logic.islist(a):
+                    exp[i+1] = Logic.convert_imp(a)
             return exp
 
     def convert_not(exp):
-        car, cdr = Logic.lisp(exp)
-        if car == Logic.op.NOT:
-            #assert(len(cdr) == 1)
-            if Logic.islist(cdr[0]):
-                exp = cdr[0]
-                car, cdr = Logic.lisp(exp)
-                if car == Logic.op.NOT:
+        """ exp is a list """
+        op, arg = Logic.lisp(exp)
+        if op.func == NOT:
+            #assert(len(arg) == 1)
+            if Logic.islist(arg[0]):
+                op2, arg2 = Logic.lisp(arg[0])
+                if op2.func == NOT:
                     ## Simplify NOT NOT
-                    return cdr[0]
-                elif car == Logic.op.AND:
-                    exp[0] = Logic.op.OR
-                    for i, e in enumerate(cdr):
-                        exp[i+1] = [Logic.op.NOT, e]
-                    return Logic.convert_not(exp)
-                elif car == Logic.op.OR:
-                    exp[0] = Logic.op.AND
-                    for i, e in enumerate(cdr):
-                        exp[i+1] = [Logic.op.NOT, e]
-                    return Logic.convert_not(exp)
+                    return arg2[0]
+                elif op2.func == AND:
+                    return OR(*map(Logic.convert_not, map(NOT, arg2)))
+                elif op2.func == OR:
+                    return AND(*map(Logic.convert_not, map(NOT, arg2)))
                 else:
-                    raise Exception("Operator '%s' should have been simplified by now"%Logic.opcode[car])
+                    raise Exception("Operator '%s' should have been simplified by now"%op2.symb)
             else:
                 ## Evaluate NOT
-                return -cdr[0]
+                return -arg[0]
         else:
-            for i, e in enumerate(cdr):
-                if Logic.islist(e):
-                    exp[i+1] = Logic.convert_not(e)
+            for i, a in enumerate(arg):
+                if Logic.islist(a):
+                    exp[i+1] = Logic.convert_not(a)
             return exp
 
     def distribute_or(exp):
         car, cdr = Logic.lisp(exp)
-        if car == Logic.op.OR:
+        if car.func == OR:
             for i, e in enumerate(cdr):
                 if Logic.islist(e):
                     car, cdr2 = Logic.lisp(e)
-                    if car == Logic.op.AND:
+                    if car.func == AND:
                         if i == 0:
                             ## distribute to the left
                             ai = cdr2
@@ -150,19 +144,19 @@ class Logic:
                             oi = cdr[2:]
                             if len(oi):
                                 reta = [0] * (1 + len(ai))
-                                reta[0] = Logic.op.AND
+                                reta[0] = AND_op
                                 for i, a in enumerate(ai):
-                                    reta[i+1] = [Logic.op.OR, a, o]
+                                    reta[i+1] = [OR, a, o]
                                 reto = [0] * (2 + len(oi))
-                                reto[0] = Logic.op.OR
+                                reto[0] = OR_op
                                 reto[1] = reta
                                 reto[2:] = oi
                                 return Logic.distribute_or(reto)
                             else:
                                 reta = [0] * (1 + len(ai))
-                                reta[0] = Logic.op.AND
+                                reta[0] = AND_op
                                 for i, a in enumerate(ai):
-                                    reta[i+1] = [Logic.op.OR, a, o]
+                                    reta[i+1] = [OR_op, a, o]
                                 return Logic.distribute_or(reta)
                         else:
                             ## distribute to the right
@@ -172,10 +166,10 @@ class Logic:
                             print(i, o, oi, ai)
                             reta = [0] * (1 + len(ai))
                             for i, a in enumerate(ai):
-                                reta[i+1] = [Logic.op.OR, *o, a]
+                                reta[i+1] = [OR_op, *o, a]
                             if len(oi):
                                 reto = [0] * (2 + len(oi))
-                                reto[0] = Logic.op.OR
+                                reto[0] = OR_op
                                 reto[1] = reta
                                 reto[2:] = oi
                                 print(reto)
@@ -192,20 +186,12 @@ class Logic:
 
     def associate_or(exp):
         car, cdr = Logic.lisp(exp)
-        if car == Logic.op.OR:
+        if car.func == OR:
             for i, e in enumerate(cdr):
                 if Logic.islist(e):
                     car, cdr2 = Logic.lisp(e)
-                    if car == Logic.op.OR:
-                        ## i is in cdr, make it point in exp
-                        i += 1
-                        j = len(cdr2)
-                        ret = [ 0 ] * (len(exp) + j - 1)
-                        ret[0] = Logic.op.OR
-                        ret[1:i] = exp[1:i]
-                        ret[i:i+j] = cdr2 ## exp[i]
-                        ret[i+j:] = exp[i+1:]
-                        return Logic.associate_or(ret)
+                    if car.func == OR:
+                        return Logic.associate_or(OR(*cdr[0:i],*cdr2,*cdr[i+1:]))
                     else:
                         exp[i+1] = Logic.associate_or(e)
             return exp
@@ -217,20 +203,12 @@ class Logic:
 
     def associate_and(exp):
         car, cdr = Logic.lisp(exp)
-        if car == Logic.op.AND:
+        if car.func == AND:
             for i, e in enumerate(cdr):
                 if Logic.islist(e):
                     car, cdr2 = Logic.lisp(e)
-                    if car == Logic.op.AND:
-                        ## i is in cdr, make it point in exp
-                        i += 1
-                        j = len(cdr2)
-                        ret = [ 0 ] * (len(exp) + j - 1)
-                        ret[0] = Logic.op.AND
-                        ret[1:i] = exp[1:i]
-                        ret[i:i+j] = cdr2 ## exp[i]
-                        ret[i+j:] = exp[i+1:]
-                        return Logic.associate_and(ret)
+                    if car.func == AND:
+                        return Logic.associate_and(AND(*cdr[0:i],*cdr2,*cdr[i+1:]))
                     else:
                         exp[i+1] = Logic.associate_and(e)
             return exp
@@ -244,24 +222,23 @@ class Logic:
         if not Logic.islist(exp):
             return True
         car, cdr = Logic.lisp(exp)
-        if car == Logic.op.AND:
+        if car.func == AND:
             for e in cdr:
                 if Logic.islist(e):
-                    car, cdr2 = Logic.lisp(e)
-                    if car != Logic.op.OR:
+                    car.func, cdr2 = Logic.lisp(e)
+                    if car.func != OR:
                         return False
                     for e2 in cdr2:
                         if Logic.islist(e2):
                             return False
             return True
-        elif car == Logic.op.OR:
+        elif car.func == OR:
             for e in cdr:
                 if Logic.islist(e):
                     return False
             return True
         else:
             return False
-
 
     def cnf(exp):
         if not Logic.islist(exp):
@@ -271,126 +248,149 @@ class Logic:
         exp = Logic.convert_not(exp)
         if not Logic.islist(exp):
             return exp
-        while not Logic.is_cnf(exp):
-            exp = Logic.distribute_or(exp)
+        #while not Logic.is_cnf(exp):
+        if 1:
+            print(STR(exp))
+            #exp = Logic.distribute_or(exp)
             exp = Logic.associate_or(exp)
             exp = Logic.associate_and(exp)
         return exp
 
-    def STR(exp):
-        if Logic.islist(exp):
-            car, cdr = Logic.lisp(exp)
-            if Logic.arity[car] == 2:
-                return '(' + Logic.STR(cdr[0]) + ' ' + Logic.symb[car] + ' ' + Logic.STR(cdr[1]) + ')'
-            elif Logic.arity[car] == 1:
-                return Logic.symb[car] + Logic.STR(cdr[0])
-            elif Logic.arity[car] == -2:
-                s = '(' + Logic.STR(cdr[0])
-                for e in cdr[1:]:
-                    s += ' ' + Logic.symb[car] + ' ' + Logic.STR(e) 
-                s += ')'
-                return s
-            else:
-                raise Exception('Unexpected arity')
-        else:
-            return str(exp)
+import unittest
+
+class TestSexpression(unittest.TestCase):
+    def test_not(self):
+        s = NOT(1)
+        print(STR(s))
+        s = Logic.cnf(s)
+        print(STR(s))
+        self.assertTrue(s == -1)
+    
+        s = NOT(NOT(1))
+        print(STR(s))
+        s = Logic.cnf(s)
+        print(STR(s))
+        self.assertTrue(s == 1)
+        
+        s = NOT(AND(1,2))
+        print(STR(s))
+        s = Logic.cnf(s)
+        print(STR(s))
+        self.assertTrue(s == OR(-1,-2))
+        
+        s = NOT(AND(-1,-2))
+        print(STR(s))
+        s = Logic.cnf(s)
+        print(STR(s))
+        self.assertTrue(s == OR(1,2))
+    
+        s = NOT(OR(1,2))
+        print(STR(s))
+        s = Logic.cnf(s)
+        print(STR(s))
+        self.assertTrue(s == AND(-1,-2))
+        
+        s = NOT(OR(-1,-2))
+        print(STR(s))
+        s = Logic.cnf(s)
+        print(STR(s))
+        self.assertTrue(s == AND(1,2))
+        
+        s = NOT(IMP(1,2))
+        print(STR(s))
+        s = Logic.cnf(s)
+        print(STR(s))
+        self.assertTrue(s == AND(1,-2))
+        
+        s = NOT(EQ(1,2))
+        print(STR(s))
+        s = Logic.cnf(s)
+        print(STR(s))
+        self.assertTrue(s == OR(AND(1,-2),AND(2,-1)))
+
+    def test_associate(self):
+        s = AND(1,AND(2,3))
+        print(STR(s))
+        s = Logic.cnf(s)
+        print(STR(s))
+        self.assertTrue(s == AND(1,2,3))
+    
+        s = AND(AND(2,3), 1)
+        print(STR(s))
+        s = Logic.cnf(s)
+        print(STR(s))
+        self.assertTrue(s == AND(2,3,1))
+    
+        s = AND(1,AND(2,3),4)
+        print(STR(s))
+        s = Logic.cnf(s)
+        print(STR(s))
+        self.assertTrue(s == AND(1,2,3,4))
+    
+        s = OR(1,OR(2,3))
+        print(STR(s))
+        s = Logic.cnf(s)
+        print(STR(s))
+        self.assertTrue(s == OR(1,2,3))
+    
+        s = OR(OR(2,3), 1)
+        print(STR(s))
+        s = Logic.cnf(s)
+        print(STR(s))
+        self.assertTrue(s == OR(2,3,1))
+    
+        s = OR(1,OR(2,3),4)
+        print(STR(s))
+        s = Logic.cnf(s)
+        print(STR(s))
+        self.assertTrue(s == OR(1,2,3,4))
+    
 
 if __name__ == '__main__':
-    s = Logic.AND(1,2,3)
+    unittest.main()
+
+    s = AND(1,2,3)
     print(s)
-    print(Logic.STR(s))
-    s = Logic.AND(1,Logic.OR(2,3),-4,-5)
+    print(STR(s))
+
+    s = AND(1,OR(2,3),-4,-5)
     print(s)
-    print(Logic.STR(s))
+    print(STR(s))
+
     try:
-        s = Logic.NOT(1,-5)
+        s = NOT(1,-5)
     except:
         print('Raised as expected')
 
-    s = Logic.AND(1,Logic.AND(2,3))
-    print(s)
-    print(Logic.STR(s))
+    s = OR(AND(2,3), 1)
+    print(STR(s))
     s = Logic.cnf(s)
-    print(Logic.STR(s))
+    print(STR(s))
 
-    s = Logic.AND(Logic.AND(2,3), 1)
-    print(s)
-    print(Logic.STR(s))
+    s = OR(1,AND(2,3))
+    print(STR(s))
     s = Logic.cnf(s)
-    print(Logic.STR(s))
+    print(STR(s))
 
-    s = Logic.AND(1,Logic.AND(2,3),4)
-    print(s)
-    print(Logic.STR(s))
+    s = OR(1,AND(2,3),4)
+    print(STR(s))
     s = Logic.cnf(s)
-    print(Logic.STR(s))
+    print(STR(s))
 
-    s = Logic.OR(1,Logic.OR(2,3))
-    print(s)
-    print(Logic.STR(s))
+    s = EQ(1,-2)
+    print(STR(s))
     s = Logic.cnf(s)
-    print(Logic.STR(s))
+    print(STR(s))
+    s = EQ(3,EQ(1,-2))
+    print(STR(s))
+    s = Logic.cnf(s)
+    print(STR(s))
 
-    s = Logic.OR(Logic.OR(2,3), 1)
-    print(s)
-    print(Logic.STR(s))
+    s = NOT(AND(1,-2))
+    print(STR(s))
     s = Logic.cnf(s)
-    print(Logic.STR(s))
-
-    s = Logic.OR(1,Logic.OR(2,3),4)
-    print(s)
-    print(Logic.STR(s))
+    print(STR(s))
+    s = NOT(OR(1,-2))
+    print(STR(s))
     s = Logic.cnf(s)
-    print(Logic.STR(s))
-
-    s = Logic.OR(Logic.AND(2,3), 1)
-    print(s)
-    print(Logic.STR(s))
-    s = Logic.cnf(s)
-    print(s)
-    print(Logic.STR(s))
-
-    s = Logic.OR(1,Logic.AND(2,3))
-    print(Logic.STR(s))
-    s = Logic.cnf(s)
-    print(Logic.STR(s))
-
-    s = Logic.OR(1,Logic.AND(2,3),4)
-    print(Logic.STR(s))
-    s = Logic.cnf(s)
-    print(Logic.STR(s))
-
-    s = Logic.EQ(1,-2)
-    print(s)
-    print(Logic.STR(s))
-    s = Logic.cnf(s)
-    print(Logic.STR(s))
-    s = Logic.EQ(3,Logic.EQ(1,-2))
-    print(s)
-    print(Logic.STR(s))
-    s = Logic.cnf(s)
-    print(Logic.STR(s))
-
-    s = Logic.NOT(1)
-    print(s)
-    print(Logic.STR(s))
-    s = Logic.cnf(s)
-    print(Logic.STR(s))
-    
-    s = Logic.NOT(Logic.NOT(1))
-    print(s)
-    print(Logic.STR(s))
-    s = Logic.cnf(s)
-    print(s)
-    print(Logic.STR(s))
-    
-    s = Logic.NOT(Logic.AND(1,-2))
-    print(s)
-    print(Logic.STR(s))
-    s = Logic.cnf(s)
-    print(Logic.STR(s))
-    s = Logic.NOT(Logic.OR(1,-2))
-    print(s)
-    print(Logic.STR(s))
-    s = Logic.cnf(s)
-    print(Logic.STR(s))
+    print(STR(s))
